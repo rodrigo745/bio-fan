22f2105d6ad5ee8eeae99c8233013c67765bdd89
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-22 13:04:13 -0300

    a

--- src/components/inicio/nav.jsx
+++ src/components/inicio/nav.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function Nav(){
     return(
         <div>
-            <div className="w-screen flex justify-between px-10 py-4 bg-blue-900 text-white font-medium text-sm"> {/* Contactos - parte superior */}
+            <div className="w-screen flex justify-between  px-4 md:px-10 py-4 bg-blue-900 text-white font-medium text-sm"> {/* Contactos - parte superior */}
                 <div className="flex space-x-5 "> {/* Izquierda */}
                     <div className="flex">
                         <HiOutlineMail className="m-1 scale-125 mr-2"/>
@@ -22,22 +22,23 @@ export default function Nav(){
                     <p>|</p>
                 </div>
                 <div className="flex space-x-3 cursor-pointer"> {/* Derecha */}
-                <Image src="/bandera-españa.png" width={30} height={30} alt="bandera de españa" />
-                <Image src="/bandera-cataluña.png" width={30} height={30} alt="bandera de cataluña" />
+                    <Image src="/bandera-españa.png" width={30} height={30} alt="bandera de españa" />
+                    <Image src="/bandera-cataluña.png" width={30} height={30} alt="bandera de cataluña" />
                 </div>
             </div>
 
             {/* ------------------------------------------------- */}
 
-            <div className="bg-white flex justify-between py-6 px-10 text-2xl"> {/* Nav Desktop */}
+            <div className="bg-white flex justify-between py-6 px-4 md:px-10"> {/* Nav Desktop */}
                 <div>
                     <div>
                         <Image src="/logo-bio.png" width={240} height={200} alt="Logo de bio-fan" />
                     </div>
                 </div>
-
-                <div>
-                    <div className="flex space-x-10 text-blue-900 font-bold">
+                {/* Escritorio */}
+                <div className="text-sm  md:text-lg xl:text-xl 2xl:text-2xl hidden md:block">
+                    <div className="flex text-blue-900 font-bold
+                                    space-x-2 md:space-x-3 lg:space-x-4 xl:space-x-5 2xl:space-x-10">
                         <p className={estiloHoverSecciones} >Inicio</p>
                         <p className={estiloHoverSecciones}>Aplicaciones</p>
                         <p className={estiloHoverSecciones}>Beneficios</p>
@@ -46,8 +47,22 @@ export default function Nav(){
                         <p className={estiloHoverSecciones}>Contacto</p>
                         <div className={`flex pr-5 ${estiloHoverSecciones}`}>Tienda
                             <div className="scale-150">
-                                <TiShoppingCart className="scale-150 mt-1 ml-3"/>
+                                <TiShoppingCart className="scale-150 mt-1 ml-3 scale-x-[-1.3]"/>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Movil */}
+                <div className="block md:hidden">
+                    <div className="rotate-90 text-4xl font-bold mt-1 cursor-pointer">|||</div>
+                    <div className="bg-white w-screen h-screen absolute top-0 left-0">
+                        <div className="p-10 space-y-5 text-xl font-bold text-blue-900">
+                            <p className="" >Inicio</p>
+                            <p className="">Aplicaciones</p>
+                            <p className="">Beneficios</p>
+                            <p className="">Productos</p>
+                            <p className="">Catalogo</p>
+                            <p className="">Contacto</p>
                         </div>
                     </div>
                 </div>
