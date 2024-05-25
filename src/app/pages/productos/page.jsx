81f02a5cef24419054f06583b4e8711d3b84533b
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-25 20:51:12 -0300

    a

--- src/app/pages/productos/page.jsx
+++ src/app/pages/productos/page.jsx
@@ -1,5 +1,17 @@
+import Image from "next/image"
+
 export default function Productos(){
+
+    const informacion = [
+        {}
+    ]
+
     return(
-        <div>Productos</div>
+        <div className="w-full h-[80vh]">
+            <div className="w-[25%] h-full bg-slate-500 flex content-end">
+                <h4>SERIE HDB</h4>
+                <p>Ventilador de soporte</p>
+            </div>
+        </div>
     )
 }
