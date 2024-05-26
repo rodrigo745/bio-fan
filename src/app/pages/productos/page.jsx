c7c715595a4b1c23e385d20bfa6969b8aa3f3ec1
rodrigo745 <roodrigoquinteros@gmail.com>
2024-05-25 21:07:44 -0300

    a

--- src/app/pages/productos/page.jsx
+++ src/app/pages/productos/page.jsx
@@ -3,14 +3,42 @@ import Image from "next/image"
 export default function Productos(){
 
     const informacion = [
-        {}
+        {titulo: "", descripcion: ""},
+        {titulo: "", descripcion: ""},
+        {titulo: "", descripcion: ""},
+        {titulo: "", descripcion: ""},
     ]
 
     return(
-        <div className="w-full h-[80vh]">
-            <div className="w-[25%] h-full bg-slate-500 flex content-end">
-                <h4>SERIE HDB</h4>
-                <p>Ventilador de soporte</p>
+        <div className="w-full ">
+            <div className="w-full h-[83vh] flex space-x-5">
+                {
+                    informacion.map((e, index)=> (
+                        <div key={index} className={`w-[25%] h-full 
+                        ${index == 0  && "bg-black "}
+                        ${index == 1 && "bg-red-400"}
+                        ${index == 2 && "bg-blue-400"}
+                        ${index == 3 && "bg-green-400"}
+                        flex items-end justify-center pb-10`}>
+                            <div className="p-3 text-center bg-orange-400 text-white rounded-lg">
+                                <h4 className="text-3xl">Ventildor</h4>
+                                <p className="text-lg">Ventilador de soporte</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="w-full h-[83vh] flex space-x-5 mt-10">
+                {
+                    informacion.map((e, index)=> (
+                        <div key={index} className="w-[25%] h-full bg-slate-300 flex items-end justify-center pb-10">
+                            <div className="p-3 text-center bg-orange-400 text-white rounded-lg">
+                                <h4 className="text-3xl">Ventilador</h4>
+                                <p className="text-lg">Ventilador de soporte</p>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
